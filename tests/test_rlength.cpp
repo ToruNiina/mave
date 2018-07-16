1a14e73d4270254ef9f24a82455ae17efae83e0b
ToruNiina <niina.toru.68u@gmail.com>
2018-07-16 21:07:29 +0900

    add test codes for rlength(...)

---
+++ tests/test_rlength.cpp
@@ -0,0 +1,116 @@
+#define BOOST_TEST_MODULE test_length
+#include <boost/test/included/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+
+#include <mave/vector.hpp>
+#include <mave/allocator.hpp>
+#include <tests/generate_random_matrices.hpp>
+#include <tests/tolerance.hpp>
+
+typedef boost::mpl::list<
+    mave::vector<double, 3>, mave::vector<float, 3>
+    > test_targets;
+
+constexpr std::size_t N = 120000;
+
+template<typename T, std::size_t N>
+T rlength_ref(const mave::vector<T, N>& v)
+{
+    T retval(0);
+    for(std::size_t i=0; i<N; ++i)
+    {
+        retval += v[i] * v[i];
+    }
+    return 1.0 / std::sqrt(retval);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(length_1arg, T, test_targets)
+{
+    std::mt19937 mt(123456789);
+    const auto vectors = mave::test::generate_random<T>(N, mt);
+
+    for(std::size_t i=0; i<N; ++i)
+    {
+        const auto& v = vectors.at(i);
+
+        const auto ref = rlength_ref(v);
+        const auto val = mave::rlength(v);
+        BOOST_TEST(ref == val, boost::test_tools::tolerance(
+                    mave::test::tolerance<typename T::value_type>()));
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(length_2arg, T, test_targets)
+{
+    std::mt19937 mt(123456789);
+    const auto vectors = mave::test::generate_random<T>(N, mt);
+
+    for(std::size_t i=0; i<N; i+=2)
+    {
+        const auto& v1 = vectors.at(i);
+        const auto& v2 = vectors.at(i+1);
+
+        const auto ref1 = rlength_ref(v1);
+        const auto ref2 = rlength_ref(v2);
+
+        const auto val = mave::rlength(v1, v2);
+        BOOST_TEST(ref1 == std::get<0>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+        BOOST_TEST(ref2 == std::get<1>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(length_3arg, T, test_targets)
+{
+    std::mt19937 mt(123456789);
+    const auto vectors = mave::test::generate_random<T>(N, mt);
+
+    for(std::size_t i=0; i<N; i+=3)
+    {
+        const auto& v1 = vectors.at(i);
+        const auto& v2 = vectors.at(i+1);
+        const auto& v3 = vectors.at(i+2);
+
+        const auto ref1 = rlength_ref(v1);
+        const auto ref2 = rlength_ref(v2);
+        const auto ref3 = rlength_ref(v3);
+
+        const auto val = mave::rlength(v1, v2, v3);
+        BOOST_TEST(ref1 == std::get<0>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+        BOOST_TEST(ref2 == std::get<1>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+        BOOST_TEST(ref3 == std::get<2>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(length_4arg, T, test_targets)
+{
+    std::mt19937 mt(123456789);
+    const auto vectors = mave::test::generate_random<T>(N, mt);
+
+    for(std::size_t i=0; i<N; i+=4)
+    {
+        const auto& v1 = vectors.at(i);
+        const auto& v2 = vectors.at(i+1);
+        const auto& v3 = vectors.at(i+2);
+        const auto& v4 = vectors.at(i+3);
+
+        const auto ref1 = rlength_ref(v1);
+        const auto ref2 = rlength_ref(v2);
+        const auto ref3 = rlength_ref(v3);
+        const auto ref4 = rlength_ref(v4);
+
+        const auto val = mave::rlength(v1, v2, v3, v4);
+        BOOST_TEST(ref1 == std::get<0>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+        BOOST_TEST(ref2 == std::get<1>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+        BOOST_TEST(ref3 == std::get<2>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+        BOOST_TEST(ref4 == std::get<3>(val), boost::test_tools::tolerance(
+                   mave::test::tolerance<typename T::value_type>()));
+    }
+}
