5dbc9c5ea94d730e8f50492bcdd8bf4ced3d67e8
ToruNiina <niina.toru.68u@gmail.com>
2018-07-20 20:58:04 +0900

    add one overload for generate_random function

--- tests/generate_random_matrices.hpp
+++ tests/generate_random_matrices.hpp
@@ -40,12 +40,31 @@ generate_random_matrices(std::size_t N, RNG&& rng)
     return vec;
 }
 
+template<typename T>
+struct is_matrix : std::false_type {};
+template<typename T, std::size_t R, std::size_t C>
+struct is_matrix<::mave::matrix<T, R, C>> : std::true_type {};
+
 template<typename T, typename RNG>
-std::vector<T, mave::aligned_allocator<T>>
+typename std::enable_if<is_matrix<T>::value,
+    std::vector<T, mave::aligned_allocator<T>>>::type
 generate_random(std::size_t N, RNG&& rng)
 {
     return generate_random_matrices<
         typename T::value_type, T::row_size, T::column_size>(N, rng);
+}
+
+template<typename T, typename RNG>
+typename std::enable_if<std::is_floating_point<T>::value, std::vector<T>>::type
+generate_random(std::size_t N, RNG&& rng)
+{
+    std::uniform_real_distribution<T> uni(T(-1000.0), T(1000.0));
+    std::vector<T> retval(N);
+    for(auto& v : retval)
+    {
+        v = uni(rng);
+    }
+    return retval;
 }
 
 } // test
