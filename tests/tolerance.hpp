1c0078936f2e6c1fe1bce2a196ef6fe53d0aefa1
ToruNiina <niina.toru.68u@gmail.com>
2018-07-24 23:34:33 +0900

    make tolerance strict when no approximation is used

--- tests/tolerance.hpp
+++ tests/tolerance.hpp
@@ -11,10 +11,13 @@ namespace test
 template<typename T>
 T tolerance_val();
 
-template<>
-double tolerance_val<double>(){return 1.0 / 16384.0;}
-template<>
-float tolerance_val<float>(){return 1.5f / 4096.0f;}
+#ifdef MAVE_USE_APPROXIMATION
+template<> double tolerance_val<double>(){return 1.0  / 16384.0;}
+template<> float  tolerance_val<float >(){return 1.5f / 4096.0f;}
+#else
+template<> double tolerance_val<double>(){return 1e-8;}
+template<> float  tolerance_val<float >(){return 1e-4f;}
+#endif
 
 template<typename T>
 auto tolerance() -> decltype(boost::test_tools::tolerance(std::declval<T>()))
